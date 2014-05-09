d2e005dde3a6e72ea6aa832246f64053b6c9eb70
Dillon Buchanan <thedillonb@gmail.com>
2014-05-08 23:19:12 -0400

    Added Common Code

---
+++ Xamarin.Utilities.Core/ViewModels/LoadableViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ReactiveUI;
+using System.Threading.Tasks;
+
+namespace Xamarin.Utilities.Core.ViewModels
+{
+    public abstract class LoadableViewModel : ReactiveObject
+    {
+        public IReactiveCommand LoadCommand { get; private set; }
+
+        protected LoadableViewModel()
+        {
+            LoadCommand = new ReactiveCommand();
+            LoadCommand.RegisterAsyncTask(x => Load());
+        }
+
+        public abstract Task Load();
+    }
+}
+
